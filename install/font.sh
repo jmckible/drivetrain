354cb86584c8dd70e5ca42157da8de723676d497
Jordan McKible <jordan@mckible.com>
2025-11-18 08:35:41 -0800

    Skip font install if present

--- install/font.sh
+++ install/font.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-# Install MesloLGS Nerd Font
-echo "Installing MesloLGS Nerd Font..."
-sudo pacman -S --noconfirm --needed ttf-meslo-nerd
+# Check if MesloLGS Nerd Font is already installed
+if pacman -Qi ttf-meslo-nerd &> /dev/null; then
+    echo "MesloLGS Nerd Font already installed, skipping"
+else
+    echo "Installing MesloLGS Nerd Font..."
+    sudo pacman -S --noconfirm --needed ttf-meslo-nerd
+fi
 
 # Set as default font via omarchy
 echo "Setting MesloLGS Nerd Font Mono as default font..."
