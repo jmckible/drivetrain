e373f217b148f34874e51c78f91aaebd082369a2
Jordan McKible <jordan@mckible.com>
2025-11-19 17:41:21 -0800

    Skip firefox if unecessary; try opacity

--- install/firefox.sh
+++ install/firefox.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# Install firefox from official repos
-sudo pacman -S --noconfirm --needed firefox
+# Check if firefox is already installed
+if ! pacman -Q firefox &>/dev/null; then
+    # Install firefox from official repos
+    sudo pacman -S --noconfirm --needed firefox
+fi
 
 # Set Firefox as default browser (suppress warnings if $BROWSER is already set)
 xdg-settings set default-web-browser firefox.desktop 2>/dev/null || true
