cd36b71c696be5a2cdcf022f11fddf248368c3b3
Jordan McKible <jordan@vocalvideo.com>
2026-01-01 10:20:02 -0800

    Remove snacks explorer for macos

--- stow/nvim/.config/nvim/lua/plugins/snacks-animated-scrolling-off.lua
+++ stow/nvim/.config/nvim/lua/plugins/snacks-animated-scrolling-off.lua
@@ -1,8 +1,25 @@
 return {
 	"folke/snacks.nvim",
-	opts = {
-		scroll = {
-			enabled = false, -- Disable scrolling animations
-		},
+	opts = function(_, opts)
+		-- Disable scrolling animations
+		opts.scroll = opts.scroll or {}
+		opts.scroll.enabled = false
+
+		-- Completely disable explorer - we use neo-tree
+		if opts.explorer then
+			opts.explorer = vim.tbl_deep_extend("force", opts.explorer, {
+				enabled = false,
+			})
+		else
+			opts.explorer = { enabled = false }
+		end
+
+		return opts
+	end,
+	keys = {
+		-- Disable all explorer keymaps
+		{ "<leader>e", false },
+		{ "<leader>E", false },
+		{ "<leader>fe", false },
 	},
 }
